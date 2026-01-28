995f8eea70048eab7d69296a823c940708a0d612
Tanboon Yonla-or <156587199+Tan20114@users.noreply.github.com>
2026-01-29 04:39:51 +0700

    Update CollectionManager.cs
    Modified : Re-logic Collection System

--- Assets/Tantan/Scripts/CollectionManager/CollectionManager.cs
+++ Assets/Tantan/Scripts/CollectionManager/CollectionManager.cs
@@ -12,8 +12,35 @@ public class CollectionManager : MonoBehaviour
     [SerializedDictionary("Fish Species", "Fish Caught")]
     public SerializedDictionary<LegendaryFishType, int> legendaryFishCollection;
 
-    public void AddFishToCollection(CommonFishType fish) => commonFishCollection[fish]++;
-    public void AddFishToCollection(UncommonFishType fish) => uncommonFishCollection[fish]++;
-    public void AddFishToCollection(RareFishType fish) => rareFishCollection[fish]++;
-    public void AddFishToCollection(LegendaryFishType fish) => legendaryFishCollection[fish]++;
+    public void FishCategorizedCollection(Fish fish)
+    {
+        switch (fish.fishType)
+        {
+            case FishType.Common:
+                {
+                    AddFishToCollection(fish.commonFishType);
+                    break;
+                }
+            case FishType.Uncommon:
+                {
+                    AddFishToCollection(fish.uncommonFishType);
+                    break;
+                }
+            case FishType.Rare:
+                {
+                    AddFishToCollection(fish.rareFishType);
+                    break;
+                }
+            case FishType.Legendary:
+                {
+                    AddFishToCollection(fish.legendaryFishType);
+                    break;
+                }
+        }
+    }
+
+    void AddFishToCollection(CommonFishType fish) => commonFishCollection[fish]++;
+    void AddFishToCollection(UncommonFishType fish) => uncommonFishCollection[fish]++;
+    void AddFishToCollection(RareFishType fish) => rareFishCollection[fish]++;
+    void AddFishToCollection(LegendaryFishType fish) => legendaryFishCollection[fish]++;
 }
